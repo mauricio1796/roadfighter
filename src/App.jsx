35f0d6d4c9c13202b2c9ad89fce946a7a5d33e0b
LaMaquina <ricardo.mosquerab@uniagustiniana.edu.co>
2025-03-13 01:18:53 +0000

    cambio

--- src/App.jsx
+++ src/App.jsx
@@ -56,8 +56,8 @@ export default function App() {
       {showAbout && (
         <div className="info-container">
           <p>
-            Acerca de 2: Este juego fue desarrollado para simular una carretera
-            con obstáculos. Fue creado con React.js.
+            sobre nosotros,: Este juego fue desarrollado para simular una
+            carretera con obstáculos. Fue creado con React.js.
           </p>
           <button className="close-button" onClick={() => setShowAbout(false)}>
             Cerrar
